08620158c031efe7f5ec5f9b1450fe711c63c1cb
Ivan Caro <ivancaro@MacBook-Pro-de-Ivan.local>
2024-09-26 11:31:40 -0500

    test users

--- models/internal/ref_resource.sql
+++ models/internal/ref_resource.sql
@@ -1,55 +1,12 @@
--- ref_resource.sql
+{{ config(
+    materialized="table"
+) }}
 
-with ref_resource as (
-    select * from {{ ref('your_source_table') }}
-),
-
-resource_update as (
-    select
-        r.resource_id,
-        r.resource_key,
-        r.resource_display_name,
-        r.resource_email,
-        r.position_role_id,
-        rn.row_valid_from,
-        r.row_valid_to,
-        'H' as row_status,
-        r.is_deleted,
-        r.source_system
-    from ref_resource r
-    inner join {{ ref('resource_new') }} rn
-    on r.resource_id = rn.resource_id
-    where r.row_status = 'C'
-),
-
-resource_insert as (
-    select
-        resource_id,
-        resource_key,
-        resource_display_name,
-        resource_email,
-        position_role_id,
-        row_valid_from,
-        row_valid_to,
-        row_status,
-        is_deleted,
-        source_system
-    from {{ ref('resource_new') }}
-    union all
-    select
-        resource_id,
-        resource_key,
-        resource_display_name,
-        resource_email,
-        position_role_id,
-        row_valid_from,
-        row_valid_to,
-        row_status,
-        is_deleted,
-        source_system
-    from {{ ref('resource_del') }}
-)
-
-select * from resource_update
-union all
-select * from resource_insert
+select
+    id as resource_id,
+    ifnull(full_name, '$N/A') as resource_display_name,
+    ifnull(email_address, '$N/A') as resource_email,
+    ifnull(nullif(concat(left(hex(encode("headline")), 25), right(hex(encode("headline")), 25)), ''), '-1') as position_role_id,
+    account_id
+from 
+{{ source ('in.c-cuesta-ex-mavenlink-965342437', 'users') }}
